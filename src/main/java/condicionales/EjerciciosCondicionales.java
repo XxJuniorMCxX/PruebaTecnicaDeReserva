3371952eeb7136bc3f3286eacc90a5b7e5ec6d13
junior_Miguel <juniorcastilloy@gmail.com>
2024-08-01 20:23:32 -0500

    Taller Finalizado

--- src/main/java/condicionales/EjerciciosCondicionales.java
+++ src/main/java/condicionales/EjerciciosCondicionales.java
@@ -15,39 +15,50 @@ public class EjerciciosCondicionales {
     public static void main(String[] args) {
         //Declaracion de Variables
         Scanner sc = new Scanner(System.in);
-        int fila, asiento;
+        int fila = 0, asiento = 0;
         String respuesta;
         char asientos[][] = new char[10][10];
         boolean bandera = false;
-        
+
         //Cargamos Matriz de Datos
         for (int f = 0; f < 10; f++) {
             for (int c = 0; c < 10; c++) {
                 asientos[f][c] = 'L';
             }
         }
-        
+
         //Bienvenido al Sistema
         System.out.println("******BIENVENIDO AL SISTEMA DE RESERVAS*********");
-        
-        
+
         //Reserva de Asientos
         while (bandera != true) {
-                //Visualizacion de Mapa
-                System.out.println("¿Desea Ver Los Asientos Disponible? S:Si Cualquier Otra Letra: No");
-                String verAsientos = sc.next();
-                if(verAsientos.equalsIgnoreCase(verAsientos)){
+            //Visualizacion de Mapa
+            System.out.println("¿Desea Ver Los Asientos Disponible? S:Si Cualquier Otra Letra: No");
+            String verAsientos = sc.next();
+            if (verAsientos.equalsIgnoreCase("S")) {
                 pintarMapa(asientos);
-                }
-                
-                //Reserva
-                System.out.println("");
-                System.out.println("Ingrese Filas y Asientos a Reservar");
+            }
+
+            //Reserva
+            boolean estaOk = false;
+            while (estaOk != true) {
+                System.out.println("Ingrese Fila y Asiento a Reservar");
                 System.out.print("Fila entre 0 y 9: ");
                 fila = sc.nextInt();
+
                 System.out.print("Asiento entre 0 y 9: ");
                 asiento = sc.nextInt();
-                System.out.println("No Se Permite Sobre Venta");
+                if (fila >= 0 && fila <= 9) {
+                    if (asiento >= 0 && asiento <= 9) {
+                        estaOk = true;
+                    } else {
+                        System.out.println("El numero de asiento no es válido");
+                    }
+                } else {
+                    System.out.println("El número de fila no es válido");
+                }
+
+            }
 
             if (asientos[fila][asiento] == 'L') {
                 asientos[fila][asiento] = 'X';
